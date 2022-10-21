f46863141bb8ff1b0fa5c7fe8a1742cda4fe4d13
Elizar <elizar2000mail.com@mail.ru>
2022-10-21 07:12:31 +0300

    git commit -m"git"

--- cp6.py
+++ cp6.py
@@ -1,12 +1,19 @@
-d=int(input( "меню\n 1-Плошадь треугольника \n 2-плошадь круга \n Выбирайте:"))
+while  D==a*b/2,:
+    d=int(input( "меню\n 1-Плошадь треугольника \n 2-плошадь круга \n Выбирайте:"))
 
-if d == 1 :
-    a = int(input("first: "))
-    b = int(input("second: "))
-    D=a*b/2
-    C =a + b + (a**2+b**2)**0.5
-print("S =", str(D), "P=", str(C))
+    if d == 1 :
+        m=int(input( "Далее\n 1-Плошадь треугольника \n 2-периметр треугольника \n Выбирите:"))
+        if m == 1:
+        a = int(input("first: "))
+        b = int(input("second: "))
+        D=a*b/2
+        print("S =", str(D),)
+        if m == 2:
+        v = int(input("first: "))
+        g = int(input("second: "))
+        C =v + g + (v**2+g**2)**0.5
+        print("P=", str(C))
 
-if d == 2 :
-    r = int(input("radius: "))
-print("S =", 3.14159265359*r**2)
+    if d == 2 :
+        r = int(input("radius: "))
+        print("S =", 3.14159265359*r**2)
