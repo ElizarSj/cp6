a427cb10c047528bf4867078ca1265107447f00d
ElizarSj <114694091+ElizarSj@users.noreply.github.com>
2023-10-18 17:17:56 +0300

    lab1

--- cp6.py
+++ cp6.py
@@ -1,19 +1,37 @@
-while  D==a*b/2,:
-    d=int(input( "меню\n 1-Плошадь треугольника \n 2-плошадь круга \n Выбирайте:"))
+ mkdir lab0
+cd lab0
+mkdir bibarel4
+mkdir crphish1
+touch granbull
+touch politoed0
+mkdir rampardos6
+touch roggenrola2
+cd bibarel4
+ touch geodude
+touch totodle
+touch scizor
+touch mightyena
+cd ..
+cd crphish1
+mkdir porygon
+touch mismagius
+mkdir skorupi
+cd ..
+cd rampardos6
+mkdir sceptle
+mkdir piloswine
+mkdir hypno
+touch dratini
+ cd ..
+echo "satk=4" sdef=5 spd=4 >> totodle
+echo -e "Тип диеты \n Carnivore" >> biborel/scizor
+echo -e "Способности Dark Art Intimidate Quick \n Feet" >> mightyena
+echo "ЖиветCave Forest" >> mismagius
+echo -e "Weght=107.4 height=55.0 \n atk=12 def=8" >> granbull
+echo -e "Возможности Overland=10 Surface=12 \n Underwater=6 Jump6=0 
+Power=3 Intelligence=4 Fountain=0 Gilled=0 \n PackMon=0" >> politoed0
+echo -e "weight=7.3 height=71.0 atk=6 \n def=5" >> dratini
+echo -e "Ходы block Earth Power Gravity Iron Defense Sleep \n Talk 
+Snore Stealth Rock" >> roggenrola2
 
-    if d == 1 :
-        m=int(input( "Далее\n 1-Плошадь треугольника \n 2-периметр треугольника \n Выбирите:"))
-        if m == 1:
-        a = int(input("first: "))
-        b = int(input("second: "))
-        D=a*b/2
-        print("S =", str(D),)
-        if m == 2:
-        v = int(input("first: "))
-        g = int(input("second: "))
-        C =v + g + (v**2+g**2)**0.5
-        print("P=", str(C))
 
-    if d == 2 :
-        r = int(input("radius: "))
-        print("S =", 3.14159265359*r**2)
